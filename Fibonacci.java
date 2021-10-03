a69cd1b072cfca1f41da2477a50a67e5f7375ee4
DwarsalaSivaRanjani <91799548+DwarsalaSivaRanjani@users.noreply.github.com>
2021-10-03 12:51:07 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,19 @@
+package assignment.week1.day1;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		int n = 6;
+		int a=1, b=1,c;
+		System.out.println(a);
+		System.out.println(b);
+		for(int i = 3; i<=n; i++)
+		{
+			c=a+b;
+			System.out.println(c);
+			a=b;
+			b=c;
+		}
+	}
+
+}
---
+++ PrimeNumber.java
@@ -0,0 +1,22 @@
+package assignment.week1.day1;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		int number = 13;
+		for (int i=2; i<number; i++)
+		{
+if (number%i==0)
+{
+System.out.println("13 is a prime number");
+		}
+		
+		else
+			System.out.println("13 is not a prime number");
+	}
+
+}
+}
+
+
+
